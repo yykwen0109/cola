615bf9442d049ad7ee94c85ccd8ef9dfb2459e89
yykwen0109 <yykwen0109@gmail.com>
2018-10-13 15:13:08 +0900

    first

---
+++ closet.js
@@ -0,0 +1,6 @@
+$('#menu').on('mouseenter', () => {
+	$('#menu').css('width', '15%').css('opacity','0.7');
+})
+$('#menu').on('mouseleave', () => {
+	$('#menu').css('width', '10%').css('opacity','1');
+})
