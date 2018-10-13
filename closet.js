4ae08a1291c1a38981f9e75b53e0c9e41e1ded22
yykwen0109 <yykwen0109@gmail.com>
2018-10-13 17:31:06 +0900

    4

--- closet.js
+++ closet.js
@@ -1,17 +1,17 @@
 
-<script src="https://www.gstatic.com/firebasejs/5.5.4/firebase.js"></script>
-<script>
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCDDyJREURkTMDEMLEpGWQP7uxGID7LeDA",
-    authDomain: "collaton-27bf5.firebaseapp.com",
-    databaseURL: "https://collaton-27bf5.firebaseio.com",
-    projectId: "collaton-27bf5",
-    storageBucket: "collaton-27bf5.appspot.com",
-    messagingSenderId: "610600623506"
-  };
-  firebase.initializeApp(config);
-</script>
+// <script src="https://www.gstatic.com/firebasejs/5.5.4/firebase.js"></script>
+// <script>
+//   // Initialize Firebase
+//   var config = {
+//     apiKey: "AIzaSyCDDyJREURkTMDEMLEpGWQP7uxGID7LeDA",
+//     authDomain: "collaton-27bf5.firebaseapp.com",
+//     databaseURL: "https://collaton-27bf5.firebaseio.com",
+//     projectId: "collaton-27bf5",
+//     storageBucket: "collaton-27bf5.appspot.com",
+//     messagingSenderId: "610600623506"
+//   };
+//   firebase.initializeApp(config);
+// </script>
 
 $('#menu').on('mouseenter', () => {
 	$('#menu').css('width', '15%').css('opacity','0.7');
